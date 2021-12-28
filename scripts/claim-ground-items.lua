d81e4bd1e377b4db6faeed44e5558af1f8c50b79
Aaron Sherman <ajs@ajs.com>
2021-12-28 13:36:56 -0500

    Abstract summarization of unit fields into a wrapper object
    Also address issue with procedurally gernated race names

--- scripts/claim-ground-items.lua
+++ scripts/claim-ground-items.lua
@@ -30,3 +30,167 @@ end
 if item_count > 0 then
     print("Total reclaimed items: "..item_count)
 end
+
+-- What follows is a script by PatrikLundell on the Dwarf Fortress forums.
+local enemy_civs = {}
+
+for i, diplomacy in ipairs (df.global.world.entities.all [df.global.ui.civ_id].relations.diplomacy) do
+  if df.global.world.entities.all [diplomacy.group_id].type == df.historical_entity_type.Civilization and
+     diplomacy.relation == 1 then  --  1 seems to be war, while 0 is peace for civs. Sites seem to use a different encoding.
+    table.insert (enemy_civs, dfhack.TranslateName (df.global.world.entities.all [diplomacy.group_id].name, true))
+  end
+end
+
+---------------------------
+
+function Is_Enemy_Civ (civ)
+  for i, enemy in ipairs (enemy_civs) do
+    if dfhack.TranslateName (civ.name, true) == enemy then
+      return true
+    end
+  end
+
+  return false
+end
+
+---------------------------
+
+function y ()
+  local dumped = 0
+  local dump_forbidden = 0
+  local dump_foreign = 0
+  local dump_trader = 0
+  local melt = 0
+  local trader_ground = 0
+  for i, item in ipairs (df.global.world.items.all) do
+    if item.flags.dump then
+      dumped = dumped + 1
+
+      if item.flags.forbid then
+        dump_forbidden = dump_forbidden + 1
+      end
+
+      if item.flags.foreign then
+        dump_foreign = dump_foreign + 1
+      end
+
+      if item.flags.trader then
+        dump_trader = dump_trader + 1
+      end
+    end
+
+    if item.flags.melt then
+      melt = melt + 1
+    end
+
+    if item.flags.on_ground and
+       item.flags.trader then
+      trader_ground = trader_ground + 1
+      if item.subtype.id == "ITEM_TOOL_SCROLL" then
+        item.flags.trader = false
+        dfhack.println ("Claiming scroll")
+
+      else
+        printall (item.subtype)
+      end
+    end
+  end
+
+  dfhack.println ("Dump designated: " .. tostring (dumped) .. ", Foreign: " .. tostring (dump_foreign) .. ", Trader: " .. tostring (dump_trader) .. ", Trader on the ground: " .. tostring (trader_ground) .. ", Melt designated: " .. tostring (melt))
+
+  ---------------------------
+
+  for i, unit in ipairs (df.global.world.units.all) do
+    if (dfhack.TranslateName(dfhack.units.getVisibleName(unit), true) ~= dfhack.TranslateName (unit.name, true) or
+        #unit.status.souls > 1) and
+        not unit.flags2.killed then
+      dfhack.println (dfhack.TranslateName(dfhack.units.getVisibleName(unit), true), dfhack.TranslateName (unit.name, true))
+    end
+
+    local hf = df.historical_figure.find (unit.hist_figure_id)
+
+    if not unit.flags2.killed and
+       hf and
+       hf.info.reputation then
+      if #hf.info.reputation.all_identities > 0 then
+        dfhack.color (COLOR_LIGHTRED)
+        dfhack.println ("Has false identities:", dfhack.TranslateName (unit.name, true), dfhack.TranslateName (unit.name, false), #hf.info.reputation.all_identities, hf.id)
+        for k, identity_id in ipairs (hf.info.reputation.all_identities) do
+          local identity = df.identity.find (identity_id)
+          dfhack.println ("", dfhack.TranslateName (identity.name, true), dfhack.TranslateName (identity.name, false), identity.histfig_id, df.identity_type [identity.type])
+        end
+        dfhack.color (COLOR_RESET)
+      end
+    end
+  end
+
+  local trap_count = 0
+  local spied_trap_count = 0
+  local civ_observed_count = {}
+
+  for i, building in ipairs (df.global.world.buildings.all) do
+    if building._type == df.building_trapst and
+       building.trap_type == df.trap_type.CageTrap then
+      trap_count = trap_count + 1
+
+      for k, observer in ipairs (building.observed_by_civs) do
+        if civ_observed_count [observer] then
+          civ_observed_count [observer] = civ_observed_count [observer] + 1
+
+        else
+          civ_observed_count [observer] = 1
+        end
+      end
+    end
+  end
+
+  dfhack.println ("Trap Count: " .. tostring (trap_count))
+
+  ---------------------------
+
+  for i, count in pairs (civ_observed_count) do
+    local civ = df.historical_entity.find (i)
+    if civ.race == -1 then
+--      dfhack.println ("  Traps observed by the " ..  df.historical_entity_type [civ.type] .. " "  .. dfhack.TranslateName (civ.name, true) .. ": " .. tostring (count))
+    else
+      if df.global.world.raws.creatures.all [civ.race].name [0] == "goblin" or
+         Is_Enemy_Civ (civ) then
+        dfhack.color (COLOR_LIGHTRED)
+      dfhack.println ("  Traps observed by the " .. df.global.world.raws.creatures.all [civ.race].name [0] .. " " ..
+                      df.historical_entity_type [civ.type] .. " " .. dfhack.TranslateName (civ.name, true) .. ": " .. tostring (count))
+      end
+--      dfhack.println ("  Traps observed by the " .. df.global.world.raws.creatures.all [civ.race].name [0] .. " " ..
+--                      df.historical_entity_type [civ.type] .. " " .. dfhack.TranslateName (civ.name, true) .. ": " .. tostring (count))
+      dfhack.color (COLOR_RESET)
+    end
+  end
+
+  local goblin_found = false
+
+  for i, unit in ipairs (df.global.world.units.active) do
+    local civ = df.historical_entity.find (unit.civ_id)
+
+    if civ and
+       civ.race ~= -1 and
+       (df.global.world.raws.creatures.all [civ.race].name [0] == "goblin" or
+        Is_Enemy_Civ (civ)) and
+       not unit.flags1.marauder and
+       unit.flags2.visitor and
+       dfhack.units.isAlive (unit) then
+--       not unit.flags1.invader_origin and
+--       not unit.flags1.invades and
+--       not unit.flags2.visitor_uninvited
+      if not goblin_found then
+        goblin_found = true
+        dfhack.println ()
+        dfhack.println ("Enemy civ members present:")
+      end
+
+      dfhack.color (COLOR_LIGHTRED)
+      dfhack.println (dfhack.TranslateName (unit.name, true), dfhack.TranslateName (unit.name, false), dfhack.TranslateName (civ.name, true))
+      dfhack.color (COLOR_RESET)
+    end
+  end
+end
+
+y ()
--- scripts/show-imposters.lua
+++ scripts/show-imposters.lua
@@ -53,82 +53,118 @@ local function titlecase(str)
     return str:gsub("(%a)(%a+)", function(a, b) return string.upper(a) .. string.lower(b) end)
 end
 
+local function get_race(unit)
+    ----
+    -- Like dfhack.units.getRaceName, but that function does not work
+    -- well on procedurally generated race names at this time.
+    -- If there is a bug-fix in the future, just swap which line is
+    -- commented.
+
+    -- return dfhack.units.getRaceName(unit)
+    return df.creature_raw.find(unit.race).name[0]
+end
+
+local function summarize_unit(unit)
+    ----
+    -- Return an object that gets some of the mess of unit interaction out of the way
+
+    local hist_figure = df.historical_figure.find(unit.hist_figure_id) or {name = nil}
+    local historical_name = nil
+    if hist_figure and hist_figure.name then
+        historical_name = dfhack.TranslateName(hist_figure.name)
+    end
+
+    local alias = dfhack.units.getIdentity(unit)
+    local alias_name = nil
+    if alias then
+        alias_name = dfhack.TranslateName(alias.name)
+    end
+
+    return {
+        unit = unit,
+        unit = unit.id,
+        alias = alias,
+        alias_name = alias_name,
+        active = dfhack.units.isActive(unit),
+        age = round(dfhack.units.getAge(unit), 1),
+        alive = dfhack.units.isAlive(unit),
+        anti_life = dfhack.units.isOpposedToLife(unit),
+        bloodsucker = dfhack.units.isBloodsucker(unit),
+        citizen = dfhack.units.isCitizen(unit),
+        crazed = dfhack.units.isHidingCurse(unit),
+        hidden_curse = dfhack.units.isHidingCurse(unit),
+        historical_name = historical_name,
+        nemesis = dfhack.units.getNemesis(unit),
+        race_name = titlecase(get_race(unit)),
+        real_age = round(dfhack.units.getAge(unit, true), 1),
+        real_name = dfhack.TranslateName(unit.name),
+        soul_name = dfhack.TranslateName(unit.status.current_soul.name),
+    }
+
+end
+
 local function usage()
     -----
     -- Print usage message
 
     print("Usage: show-imposters [-living] [-dead] [-vampire] [-inactive]")
+    print("  Checks selected unit or all units if none selected.")
     print("  -living - show only living imposters")
     print("  -dead - show only dead imposters")
     print("  -vampire - show only vampires")
     print("  -inactive - include inactive units")
 end
 
-local function print_alias_info(unit, alias)
+local function print_alias_info(unit)
     -----
-    -- Given a unit and its alias, print the summary
+    -- Given an imposter unit summary, format the info for the user
 
-    local alias_name = dfhack.TranslateName(alias.name)
-    local race_name = titlecase(dfhack.units.getRaceName(unit))
-    local real_name = dfhack.TranslateName(unit.name)
-    local historical_name = dfhack.TranslateName(df.historical_figure.find(unit.hist_figure_id).name)
-    local soul_name = dfhack.TranslateName(unit.status.current_soul.name)
-    local nemesis = dfhack.units.getNemesis(unit)
-    local hidden_curse = dfhack.units.isHidingCurse(unit)
-    local crazed = dfhack.units.isHidingCurse(unit)
-    local anti_life = dfhack.units.isOpposedToLife(unit)
-    local active = dfhack.units.isActive(unit)
-    local citizen = dfhack.units.isCitizen(unit)
-    local age = round(dfhack.units.getAge(unit), 1)
-    local real_age = round(dfhack.units.getAge(unit, true), 1)
-    local alive = dfhack.units.isAlive(unit)
-    local bloodsucker = dfhack.units.isBloodsucker(unit)
     local prefix = "   - * "  -- Output prefix for detail info
 
-    print("** Imposter found! Current alias: "..alias_name.." ("..race_name..")")
-    print("   - "..real_name.." is their real name.")
-    if not historical_name == real_name and not historical_name == alias_name then
-        print("   - "..historical_name.." is their historical name.")
-    end
-    if not soul_name == real_name and not soul_name == alias_name then
-        print("   - "..soul_name.." is their soul name.")
-    end
+    print("** Imposter found! Current alias: "..unit.alias_name.." ("..unit.race_name..")")
+    print("   - "..unit.real_name.." is their real name.")
     print("   - Additionally:")
-    if nemesis and not nemesis.unit_id == unit.id then
-        local nemesis_unit = df.unit.find(nemesis.unit_id)
+    if unit.historical_name and unit.historical_name ~= unit.real_name and unit.historical_name ~= unit.alias_name then
+        print(prefix..unit.historical_name.." is their historical name.")
+    end
+    if unit.soul_name and unit.soul_name ~= unit.real_name and unit.soul_name ~= unit.alias_name then
+        print(prefix..unit.soul_name.." is their soul name.")
+    end
+    if unit.nemesis and unit.nemesis.unit_id ~= unit.id then
+        local nemesis_unit = df.unit.find(unit.nemesis.unit_id)
         local nemesis_name = dfhack.TranslateName(dfhack.units.getVisibleName(nemesis_unit))
         print(prefix.."has a nemesis: "..nemesis_name)
     end
-    if hidden_curse then
+    if unit.hidden_curse then
         print(prefix.."hides the nature of their curse")
     end
-    if crazed then
+    if unit.crazed then
         print(prefix.."crazed")
     end
-    if anti_life then
+    if unit.anti_life then
         print(prefix.."opposed to life")
     end
-    if bloodsucker then
+    if unit.bloodsucker then
         print(prefix.."drinks blood")
     end
-    if not active then
+    if not unit.active then
         print(prefix.."inactive")
     end
-    if alive then
+    if unit.alive then
         print(prefix.."alive")
     else
         print(prefix.."dead ('your friend is only mostly dead')")
     end
     local citizen_status = "visitor"
-    if citizen then
+    if unit.citizen then
         citizen_status = "citizen"
     end
     print(prefix..citizen_status)
     local age_tag = "age"
-    local age_value = age
-    if not age == real_age then
+    local age_value = unit.age
+    if not unit.age == unit.real_age then
         age_tag = "age (real)"
-        age_value = age.." ("..real_age..")"
+        age_value = unit.age.." ("..unit.real_age..")"
     end
     print(prefix..age_tag..": "..age_value)
     print(" ")
@@ -140,42 +176,63 @@ local function main(...)
 
     local utils = require 'utils'
 
-    local valid_opts = utils.invert({"help", "living", "dead", "vampire", "inactive"})
-    local args = utils.processArgs({...}, valid_opts)
+    local valid_opts = utils.invert({"help", "living", "dead", "vampire", "inactive", "citizen"})
+    local args
+    local invocation = {...}
+    local status, result = pcall(function () return utils.processArgs(invocation, valid_opts) end)
+    if status then
+        args = result
+    else
+        print(result)
+        args = {help = true}
+    end
 
     if args.help then
         usage()
         return
     end
 
-    local only_living = args.living
-    local only_dead = args.dead
-    local only_vampire = args.vampire
-    local include_inactive = args.inactive
-
     local imp_count = 0
-    for _,unit in ipairs(df.global.world.units.all) do
-        local alive = dfhack.units.isAlive(unit)
-        local bloodsucker = dfhack.units.isBloodsucker(unit)
-        local active = dfhack.units.isActive(unit)
-        local alias = dfhack.units.getIdentity(unit)
 
-        arg_restricted = (
-            (only_living and not alive) or
-            (only_dead and alive) or
-            (only_vampire and not bloodsucker) or
-            (not include_inactive and not active)
-        )
+    local selected_unit = dfhack.gui.getSelectedUnit(true)
+    local units
 
-        if alias and not arg_restricted then
-            print_alias_info(unit, alias)
-            imp_count = imp_count + 1
+    if selected_unit then
+        units = {selected_unit}
+        selected_name = dfhack.TranslateName(dfhack.units.getVisibleName(selected_unit))
+        print("Info for selected unit, "..selected_name.."...")
+    else
+        units = df.global.world.units.all
+    end
+
+    for _,unit in ipairs(units) do
+
+        local summary = summarize_unit(unit)
+
+        if summary.alias then
+            -- All of the command-line argument filters applied:
+            arg_restricted = (
+                (args.living and not summary.alive) or
+                (args.dead and summary.alive) or
+                (args.vampire and not summary.bloodsucker) or
+                (args.citizen and not summary.citizen) or
+                (not args.inactive and not summary.active)
+            )
+
+            if not arg_restricted then
+                print_alias_info(summary)
+                imp_count = imp_count + 1
+            end
         end
     end
 
     if imp_count > 0 then
         print("Total imposters: "..imp_count)
-    end
+    else if selected_unit then
+        print("Not an imposter.")
+    else
+        print("No imposters.")
+    end end
 end
 
 if not dfhack_flags.module then
